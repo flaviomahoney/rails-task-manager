c96a2bd67299c13830dfcceca79c120978fb263c
flaviomahoney <flavio@mahoneyt.com>
2020-05-26 12:37:33 -0300

    Adding Index

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Task.create!(
+	title: "I need go to the Bathroom",
+	details: "I need to pee",
+	completed: false
+)
